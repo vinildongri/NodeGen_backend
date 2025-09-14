5944b54d0a13c9edd9061989b72b6aa36a7ea763
vinildongri <dongrivinil@gmail.com>
2025-09-14 22:34:41 +0530

    commit-2

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true })); // <-- parse URL-encoded bodies
 app.use(cookieParser());
 
 // importing all routes
